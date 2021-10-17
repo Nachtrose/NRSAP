0f21d4e36c3ec4e50c6a855fa83038eef5d8e9fc
Nachtrose <81585420+Nachtrose@users.noreply.github.com>
2021-10-17 18:57:45 +0200

    Update window-popup.js

--- scripts/window-popup.js
+++ scripts/window-popup.js
@@ -4,7 +4,7 @@ Hooks.once('ready', async function () {
         if (game.settings.get("NRSAP", "runonlyonce") == false) {
             let d = new Dialog({
                 title: "Update warning!",
-                content: `<div style="text-align: justify;"><h2>Welcome to Nachtroses's Sci-Fi ( and modern ) Animated Pack</h2><p>Due to merging in some assets from the great Sequencer module, a changed some naming conventions for the sake of consistency.</p><p>This may cause some issues to anyone, who was using this module already.</p><p>I'm deeply sorry for the inconvenience and goingg forward, i will strive to do similar module-wide changes as rare as possible. Ideally none.</p>`,
+                content: `<div style="text-align: justify;"><h2>Welcome to Nachtroses's Sci-Fi ( and modern ) Animated Pack</h2><p>Due to merging in some assets from the great Sequencer module, a changed some naming conventions for the sake of consistency.</p><p>This may cause some issues to anyone who was using this module already.</p><p>I'm deeply sorry for the inconvenience and going forward, i will strive to do similar module-wide changes as rare as possible. Ideally none.</p>`,
                 buttons: {
                     one: {
                         icon: '<i class="fas fa-clipboard-list"></i>',
